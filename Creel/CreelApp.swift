cf1065a63376a74d90c3d98b675d12c69460cd05
aareb <akchowdhury@umass.edu>
2025-06-17 23:12:49 -0400

    Initial Commit

---
+++ Creel/CreelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreelApp.swift
+//  Creel
+//
+//  Created by Aareb Chowdhury on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CreelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreelTests/CreelTests.swift
@@ -0,0 +1,17 @@
+//
+//  CreelTests.swift
+//  CreelTests
+//
+//  Created by Aareb Chowdhury on 6/17/25.
+//
+
+import Testing
+@testable import Creel
+
+struct CreelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
